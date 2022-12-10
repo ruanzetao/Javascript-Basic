5f23e4dc355ed0e17c0d8b707049ebcc7adf55e2
ruanzetao <ruanzetao@gmail.com>
2022-12-10 16:09:38 +0700

    JS: init work with Array

--- L22_WorkWithNumber.js
+++ L22_WorkWithNumber.js
@@ -1,1 +1,26 @@
-//alert('Hi Javascript basic!');
+//Kiểu số (Number) trong Javascript
+
+// 1. Tạo giá trị Number
+// - Các cách Tạo
+// - Dùng cách nào? Tại sao?
+// - Kiểm tra Data Type
+
+// 2. Làm việc với Number
+// - toString();
+// - toFixed();
+
+// Keyword: Javascript Number methods
+var age = 18;
+var PI = 3.14;
+
+var otherNumer = new Number(9);
+
+var result = 20 / "ACB";
+console.log(result);
+
+console.log(isNaN(result));
+
+console.log(PI.toString());
+
+var myAge = age.toString();
+console.log(myAge);
---
+++ L23_Array.js
@@ -0,0 +1,14 @@
+// MẢNG TRONG JAVASCRIPT
+
+// 1. Tạo Mảng
+// 2. Truy xuất Mảng
+// - Độ dài mảng
+// - Lấy phần tử theo Index
+
+var languages = ["Javascript", "PHP", "Ruby"];
+
+console.log(languages);
+
+console.log(Array.isArray(languages));
+
+console.log(languages[0]);
