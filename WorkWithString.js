78e3befec7745645dc71b2f537cdb7463466c9b0
ruanzetao <ruanzetao@gmail.com>
2022-12-10 11:54:24 +0700

    JS: work with String

---
+++ WorkWithString.js
@@ -0,0 +1,13 @@
+//LÀM VIỆC VỚI CHUỖI
+
+
+// 1. Length
+// 2. Find index
+// 3. Cut String
+// 4. Replace
+// 5. Convert to upper case
+// 6. Convert to lower case
+// 7. Trim
+// 8. Split
+// 9. Get a character by index
+
