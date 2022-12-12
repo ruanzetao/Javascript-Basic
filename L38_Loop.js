9e2c3509eb842fc6aee9894e17c72aea0e9eeb48
ruanzetao <ruanzetao@gmail.com>
2022-12-13 00:30:55 +0700

    JS: init lesson for Loop

---
+++ L38_Loop.js
@@ -0,0 +1,8 @@
+//Lesson: Vòng lặp - Loop
+// 1. for - lặp với điều kiện đúng
+// 2. for/in - lặp qua key của đối tượng
+// 3. for/of - lặp qua value của đối tượng
+// 4. while - Lặp khi điều kiện đúng
+// 5. do/while
+
+console.log("");
