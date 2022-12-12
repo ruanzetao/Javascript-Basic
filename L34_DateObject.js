19c3565540d4c1c1180f1b55728fb4115d3982fb
ruanzetao <ruanzetao@gmail.com>
2022-12-12 22:24:49 +0700

    JS: ternary operator

--- L34_DateObject.js
+++ L34_DateObject.js
@@ -1,3 +1,9 @@
 //Lesson: Date object
+//Keyword: javascript Date object mozilla
+
 var date = new Date();
 console.log(date);
+
+var year = date.getFullYear();
+var month = date.getMonth();
+var date = date.getDate();
---
+++ L37_TernaryOperator.js
@@ -0,0 +1,17 @@
+//Lesson: Ternary Operator
+
+console.log("");
+
+var course = {
+  name: "Javascript",
+  coin: 280,
+};
+
+if (course.coin > 0) {
+  console.log(`${course.coin} Coins`);
+} else {
+  console.log("Free");
+}
+
+//Ternay Operator
+var result = course.coin > 0 ? `${course.coin} Coins` : "Free";
