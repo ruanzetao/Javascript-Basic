506bbb2ea012f27a4edd82c3c0c4d6c4414a91e1
ruanzetao <ruanzetao@gmail.com>
2022-12-12 09:43:00 +0700

    JS: type of Function in JS

--- L28_FunctionExtra.js
+++ L28_FunctionExtra.js
@@ -1,4 +1,4 @@
 //Lesson:
 // 1. Khi 1 function đặt cùng tên
-// 2. Khai báo biến trong h àm
-// 3. Định nghĩa hàm trong hàm
+// 2. Khai báo biến trong hàm
+// 3. Định nghĩa hàm trong hàm => Có.
---
+++ L29_TypesOfFunctions.js
@@ -0,0 +1,14 @@
+//Lesson: Các loại function
+//1. Declaration function
+//2. Expression function
+//3. Arrow function
+
+console.log("Something.");
+
+var showMessage2 = function testName() {};
+
+setTimeout(function autoLogin() {});
+
+var myObject = {
+  myFunction: function testName() {},
+};
