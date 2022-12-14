50fb29594433d86bbf3fc86e54b636187b9ed7fc
ruanzetao <ruanzetao@gmail.com>
2022-12-14 11:37:34 +0700

    JS: forin and forof in JS

---
+++ L42_ForIn.js
@@ -0,0 +1,11 @@
+//Lesson: For/In
+
+var myInfo = {
+  name: "Sang Nguyen",
+  age: 18,
+  address: "HCMC, Vietnam",
+};
+
+for (var key in myInfo) {
+  console.log(key);
+}
---
+++ L42_ForOf.js
@@ -0,0 +1,7 @@
+//Lesson: For/In
+
+var languages = ["Javascript", "PHP", "Java"];
+
+for (var value of languages) {
+  console.log(value);
+}
