5b811a481b6535e31e0389d598d071b9897100f3
ruanzetao <ruanzetao@gmail.com>
2022-12-15 07:58:23 +0700

    JS: Array methods

--- L42_ForOf.js
+++ L42_ForOf.js
@@ -2,6 +2,15 @@
 
 var languages = ["Javascript", "PHP", "Java"];
 
+var myInfo = {
+  name: "Sang Nguyen",
+  age: 18,
+  address: "HCMC, Vietnam",
+};
 for (var value of languages) {
+  console.log(value);
+}
+
+for (var value of Object.values(myInfo)) {
   console.log(value);
 }
---
+++ L49_ArrayMethods.js
@@ -0,0 +1,24 @@
+// MẢNG TRONG JAVASCRIPT
+
+/*
+1. forEach
+2. every : boolean
+3. some
+4. find 
+5. filter
+6. map
+7. reduce
+*/
+
+var courses = [
+  {
+    id: 1,
+    name: "HTML, CSS",
+    coin: 100,
+  },
+  {
+    id: 2,
+    name: "Javascript",
+    coin: 200,
+  },
+];
