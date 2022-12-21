2744b79a9e6772f06197d4a7a9200fd7334bb796
ruanzetao <ruanzetao@gmail.com>
2022-12-21 11:34:01 +0700

    JS: event Listener

---
+++ L79_EventListener.js
@@ -0,0 +1,18 @@
+//Lesson: Event listener
+/*
+1. Event Listener
+2. Json
+3. Fetch
+4. DOM location
+5. Local storage
+6. Session storage
+7. Coding convention
+8. Best practices
+9. Mistakes
+10. Performance
+
+1. Xử lý nhiều việc khi 1 event xảy ra
+2. Lắng nghe/ Hủy bỏ lắng nghe
+*/
+
+console.log("");
