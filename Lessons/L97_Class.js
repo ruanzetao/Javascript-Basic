921cfa1cc507de783f8315e562ef3c5d42214358
ruanzetao <ruanzetao@gmail.com>
2023-01-08 15:40:10 +0700

    JS: init default Parameters

--- Lessons/L97_Class.js
+++ Lessons/L97_Class.js
@@ -1,3 +1,10 @@
 //Lesson: Class
 
+class Course{
+    constructor(name, price) {
+        this.name=name;
+        this.price=price;
+    }
+}
+
 console.log("");
---
+++ Lessons/L98_DefaultParameters.js
@@ -0,0 +1,11 @@
+//Lesson:
+
+console.log("");
+
+function logger(log) {
+  if (typeof log === "undefined") {
+    log = "Gia tri mac dinh";
+  }
+
+  console.log(log);
+}
