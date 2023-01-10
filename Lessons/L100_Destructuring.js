20564020993f37ff32b6db4332f322be21083f31
ruanzetao <ruanzetao@gmail.com>
2023-01-10 21:24:47 +0700

    JS: destructuring in JS

---
+++ Lessons/L100_Destructuring.js
@@ -0,0 +1,13 @@
+//Lesson:
+
+console.log("");
+var array = ["Javascript", "PHP", "Ruby"];
+var [a, b, c] = array;
+var [a, ...rest] = array;
+
+var course = {
+  name: "JavaScript",
+  price: 1000,
+};
+
+var { name, price } = course;
